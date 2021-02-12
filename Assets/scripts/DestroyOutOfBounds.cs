1d1f2f5f88da86728d102ee31b5b09cfdfe0aa4f
MgStorm Ok <nicoanriquez23@gmail.com>
2021-02-11 22:41:35 -0300

    first Version of Wild Life
    some lines of code, move controller, spawn and destroy projectiles and enemies

---
+++ Assets/scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 35f;
+    private float lowerBound = -35f;
+       
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.position.y>topBound || this.transform.position.y < lowerBound)
+        { 
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed, horizontalInput, verticalInput;
+    public float xRange = 19.0f;
+    public float yRange = 9.85f;
+
+    public GameObject projectilePrefab;
+   
+    // Update is called once per frame
+    void Update()
+    {   
+        //movimiento del Pj
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        this.transform.Translate(speed * Time.deltaTime * Vector3.right * horizontalInput);
+        this.transform.Translate(speed * Time.deltaTime * Vector3.forward * verticalInput);
+
+        CheckLimits(xRange, yRange);
+
+        //Acciones del Pj
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //si entramos aca, hay que lanzar un proyectil
+            Instantiate(projectilePrefab,
+                        transform.position, 
+                        projectilePrefab.transform.rotation);
+        }
+    }
+    public void CheckLimits(float x, float y)
+    {
+
+        if (transform.position.x < -x)
+        {
+            //se sale a izq de la pantalla
+            transform.position = new Vector3(x: -x, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > x)
+        {
+            //se sale a der de la pantalla
+            transform.position = new Vector3(x: x, transform.position.y, transform.position.z);
+        }
+        if (transform.position.y < -y)
+        {
+            //se sale arriba de la pantalla
+            transform.position = new Vector3(transform.position.x, y: -y, transform.position.z);
+        }
+        if (transform.position.y > y)
+        {
+            //se sale arriba de la pantalla
+            transform.position = new Vector3(transform.position.x, y: y, transform.position.z);
+        }
+
+    }
+}
