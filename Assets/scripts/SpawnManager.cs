6f155156be59b59604c908ad02489351cdf5e655
MgStorm Ok <nicoanriquez23@gmail.com>
2021-02-12 00:56:11 -0300

    Player Controller and Spawn Enemies
    - Speed of enemies variated
    - player controlling spawn enemies.
    - spawn manager check

--- Assets/scripts/SpawnManager.cs
+++ Assets/scripts/SpawnManager.cs
@@ -6,17 +6,34 @@ public class SpawnManager : MonoBehaviour
 {
 
     public GameObject[] enemies;
-
-
-    // Start is called before the first frame update
-    void Start()
+    public int animalIndex;
+    [SerializeField]
+    private float spawnRangeX = 25;
+    private float spawnPosY;
+    private void Start()
     {
-        
+        spawnPosY = this.transform.position.y;
     }
-
     // Update is called once per frame
     void Update()
     {
         
+
+        if (animalIndex >= 0 && animalIndex < enemies.Length)
+        {
+            //el animal existe
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                //posicion de generación de enemigos.
+                float xRand = Random.Range(-spawnRangeX, spawnRangeX);
+                animalIndex = Random.Range(0, enemies.Length);
+                Vector3 spawnPos = new Vector3(xRand, spawnPosY, 0);
+                Instantiate(enemies[animalIndex], spawnPos, enemies[animalIndex].transform.rotation);
+            }
+        }
+        else
+        {
+            Debug.LogError("el Índice del enemigo a instanciar NO existe");
+        }
     }
 }
